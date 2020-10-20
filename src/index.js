38f8d43362a01247ff8d94ff1d9bcbcf4b6abcac
ivanpolyakov99 <ivan.polyakovgds@gmail.com>
2020-10-20 10:52:53 +0300

    feat: add functional

--- src/index.js
+++ src/index.js
@@ -2,5 +2,13 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  if (matrix === undefined) {
+    return []; 
+  } else {
+    function reducer (first, second, step) {
+      return first.concat(step % 2 === 0 ? second : second.reverse())
+    }
+  
+    return matrix.reduce(reducer, [])
+  }
 }
